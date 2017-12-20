55dcee037b420545f4c8835e92dbc52870bf9364
gabangel <ezenwankwogabriel@gmail.com>
2017-12-20 15:28:56 +0100

    add login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
+import { ShoppingItemComponent } from './shopping-item/shopping-item.component';
+import { AuthLoginComponent } from './auth-login/auth-login.component';
+import { AuthSignupComponent } from './auth-signup/auth-signup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingItemComponent,
+    AuthLoginComponent,
+    AuthSignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DataService {
+
+  constructor(private http: Http) { }
+
+
+  checkUser(user){
+    return this.http.get('/api/authenticate')
+    .map(res=> res.json())
+    }
+
+  addNewUser(newItem){
+    let headers = new Headers();
+    headers.append('content-type', 'application/json');
+    return this.http.post('/api/setup', newUser,   {headers: headers})
+      //map the return from the post to be a json file
+      .map(res => res.json())
+  }
+  
+  
+}
