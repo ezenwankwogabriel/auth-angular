c234f00edc4aa8d6f43d9efabef7c403a6ec9560
gabangel <ezenwankwogabriel@gmail.com>
2017-12-22 06:22:43 +0100

    add READme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule }from '@angular/http';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { LoginAuthComponent } from './login-auth/login-auth.component';
+import { HomeComponent } from './home/home.component';
+
+const ROUTES: Routes = [
+  {path: '' , component:LoginAuthComponent},
+  {path: 'home', component: HomeComponent}
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginAuthComponent
+    LoginAuthComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
